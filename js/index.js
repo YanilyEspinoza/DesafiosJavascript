b978184316f59ee6e13fc23ff83d6b6517859560
Yanily Espinoza <yanilyespinozaarg@gmail.com>
2022-09-06 22:04:31 -0300

    Tercer Desafío Arrays

--- js/index.js
+++ js/index.js
@@ -1,90 +1,37 @@
-//Ejercicio: Calcular el pago a realizar según intereses y cuotas a pagar.
+//Desafío Entregable 3
 
-//Constantes
-const IVA = 0.21
-const CUOTABR = 0.10
-const CUOTAMR = 0.20
-const CUOTAAR = 0.30
+//Constante
+const NUEVOID = ()=> parseInt(Math.random() * 1000000)
 
-//Información solicitada
-let montoCreditoSolicitado = parseInt(prompt("Ingrese el monto del crédito a solicitar"));
+//Array Productos
+const Productos = [] 
 
-while (montoCreditoSolicitado < 10000 || montoCreditoSolicitado > 100000) {
-    alert ("Debe ingresar un monto mayor a 10.000$ y menor a 100.000$");
-    montoCreditoSolicitado = parseInt(prompt("Ingrese el monto del crédito a solicitar"));
-}
 
-let numeroCuotas = parseInt(prompt("Ingrese la cantidad de Cuotas deseadas"))
-
-while (numeroCuotas < 6 || numeroCuotas > 12) {
-    alert ("Debe seleccionar entre 6 a 12 cuotas");
-    numeroCuotas = parseInt(prompt("Ingrese la cantidad de Cuotas deseadas"));
-}
-
-//Cálculos
-
-let creditoIVA = (montoCreditoSolicitado * IVA) + montoCreditoSolicitado
-
-switch (numeroCuotas){
-    case (6):
-        let calculoBR1 = (creditoIVA * CUOTABR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoBR1 + " pesos, con pagos mensuales de: " + (calculoBR1/6) + " pesos." );
-        break;
-    case (7):
-        let calculoBR2 = (creditoIVA * CUOTABR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoBR2 + " pesos, con pagos mensuales de: " + (calculoBR2/7) + " pesos." );
-        break;
-    case (8):
-        let calculoMR1 = (creditoIVA * CUOTAMR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoMR1 + " pesos, con pagos mensuales de: " + (calculoMR1/8) + " pesos." );
-        break;
-    case (9):
-        let calculoMR2 = (creditoIVA * CUOTAMR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoMR2 + " pesos, con pagos mensuales de: " + (calculoBR/9) + " pesos." );
-        break;
-    case (10):
-        let calculoAR1 = (creditoIVA * CUOTAAR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoAR1 + " pesos, con pagos mensuales de: " + (calculoAR1/10) + " pesos." );
-        break;
-    case (11):
-        let calculoAR2 = (creditoIVA * CUOTAAR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoAR2 + " pesos, con pagos mensuales de: " + (calculoAR2/11) + " pesos." );
-        break; 
-    case (12):
-        let calculoAR3 = (creditoIVA * CUOTAAR) + creditoIVA
-        alert ("El monto total a pagar será de: " + calculoAR3 + " pesos, con pagos mensuales de: " + (calculoAr3/12) + " pesos." );
-        break;
-}
-
-function nuevoCalculo (montoCreditoSolicitado , numeroCuotas) {
-    switch (numeroCuotas) {
-        case (6):
-            let calculoBR1 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTABR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoBR1 + " pesos, con pagos mensuales de: " + (calculoBR1/6) + " pesos." );
-            break;
-        case (7):
-            let calculoBR2 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTABR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoBR2 + " pesos, con pagos mensuales de: " + (calculoBR2/7) + " pesos." );
-            break;
-        case (8):
-            let calculoMR1 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTAMR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoMR1 + " pesos, con pagos mensuales de: " + (calculoMR1/8) + " pesos." );
-            break;
-        case (9):
-            let calculoMR2 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTAMR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoMR2 + " pesos, con pagos mensuales de: " + (calculoBR/9) + " pesos." );
-            break;
-        case (10):
-            let calculoAR1 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTAAR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoAR1 + " pesos, con pagos mensuales de: " + (calculoAR1/10) + " pesos." );
-            break;
-        case (11):
-            let calculoAR2 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTAAR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoAR2 + " pesos, con pagos mensuales de: " + (calculoAR2/11) + " pesos." );
-            break; 
-        case (12):
-            let calculoAR3 = (((montoCreditoSolicitado * IVA) + montoCreditoSolicitado) * CUOTAAR) + ((montoCreditoSolicitado * IVA) + montoCreditoSolicitado)
-            alert ("El monto total a pagar será de: " + calculoAR3 + " pesos, con pagos mensuales de: " + (calculoAr3/12) + " pesos." );
-            break;
+// Clase constructora de producto
+class Producto {
+    constructor(id, nombre, medida, color, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.medida = medida
+        this.color = color
+        this.precio = precio
     }
+}
+
+function agregarProducto() {
+    let id = NUEVOID()
+    let nombre = prompt("Ingresa la descripción del producto:")
+    let medida = prompt("Ingresa la medida del producto:")
+    let color = prompt("Ingresa el color del producto:")    
+    let precio = prompt("Ingresa el precio del producto:")
+    debugger
+    const Produts = {id: id, nombre: nombre, medida: medida, color: color, precio: precio}
+        Productos.push(Produts)
+        console.table(Productos)
+}
+
+function dataBase() {
+	Productos.push(new Producto(5875, "Globo numérico", 5, "rojo", 5000))
+	Productos.push(new Producto(5876, "Globo de agua", 4, "verde", 3000))
+	Productos.push(new Producto(5877, "Globo payaso", 3, "amarillo", 3500))
 }
